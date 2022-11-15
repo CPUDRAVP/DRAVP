fc10da6db410020792671f453ec60b1414204661
CPUDRAVP <116559444+CPUDRAVP@users.noreply.github.com>
2022-11-15 13:24:18 +0800

    Delete DRAVP_source_code/Structure directory

--- DRAVP_source_code/Structure/download.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$filename = $_GET['filename'];
-header('content-disposition:attachment;filename='.basename($filename));
-header('Content-Length:'.filesize($file));
-readfile($filename);
-
-?>
